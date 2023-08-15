14dea7052cfa387f8c269bfe26b8d00308440bcd
rogersitorus <140483579+rogersitorus@users.noreply.github.com>
2023-08-15 13:38:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const navbarNav = document.querySelector('.hero-navbar .hero-navbar-nav')
+function tampilNavbar(params) {
+     const navbarNav = document.querySelector('.hero-navbar .hero-navbar-nav')
+     document.querySelector('.hero-navbar-menu').onclick = () => {
+         navbarNav.classList.toggle('active')
+     }
+ }
 
-document.querySelector('.hero-navbar-menu').onclick = () => {
-    navbarNav.classList.toggle('active')
-}   
+ function tampilCart(params) {
+     const navbarCart = document.querySelector('.navbar-sidebar')
+     document.querySelector('.nav-shop-cart').onclick = () => {
+         navbarCart.classList.toggle('active')
+     }
+ }
